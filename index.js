246cae13cb6896d1db323cef8e0cf2054fc0c6e4
denovaa <137181691+denovaa@users.noreply.github.com>
2023-12-07 16:19:27 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require ('express');
+const app = express();
+app.get ('/') , (req, res) => res.json ({message: 'Hello, world'}));
+app.listen (3030, () => console.log ('App started'));
